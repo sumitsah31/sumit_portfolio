67ffa5bcc59dbac0063de182774fdf24c586562d
Sumit Kumar Sah <sumitdashing1@gmail.com>
2024-09-08 15:43:01 +0530

    base added to vit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig } from "vite"
 
 export default defineConfig({
 	plugins: [TanStackRouterVite(), react()],
+	base: "/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
